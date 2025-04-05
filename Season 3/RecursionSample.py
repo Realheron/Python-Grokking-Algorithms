3c8e460b5d77e376ffbbae41180276c4ac16fcf6
Heron <theheron666@gmail.com>
2025-04-05 17:21:48 +0330

    Update RecursionSample.py

--- Season 3/RecursionSample.py
+++ Season 3/RecursionSample.py
@@ -1,1 +1,11 @@
+def look_for_key(box):
+    for item in box:
+        if isinstance(item, list):  
+            look_for_key(item)      
+        elif item == "key":        
+            print("Found the key!")
 
+
+my_box = ["item1", ["item2", ["key"]], "item3"]
+
+look_for_key(my_box)
