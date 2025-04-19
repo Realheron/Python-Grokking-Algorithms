0d8bc648d084e9a89a0a7046ca78d11bd56c039d
Heron <theheron666@gmail.com>
2025-04-19 14:27:01 +0330

    Update 4.3.py

--- Season 4 /4.3.py
+++ Season 4 /4.3.py
@@ -1,1 +1,10 @@
 
+def find_max_recursive(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else :
+        max_rest = find_max_recursive(lst[1:])
+        return lst[0] if lst [0] > max_rest else max_rest
+    
+
+print(find_max_recursive([4, 10, 2, 33, 5]))
